cbb4bc992eb48672336a121ad29ac634a32d8c44
jose-pr <dev@alacan.me>
2020-05-23 00:09:47 +0300

    Create wireguard_pfsense.sh

---
+++ wireguard_pfsense.sh
@@ -0,0 +1,3 @@
+pkg add -f http://pkg.freebsd.org/FreeBSD:11:amd64/latest/All/wireguard-go-0.0.20200320.txz
+pkg add -f http://pkg.freebsd.org/FreeBSD:11:amd64/latest/All/wireguard-1.0.20200513.txz
+pkg add -f https://github.com/Ascrod/pfSense-pkg-wireguard/releases/download/v1.0.0/pfSense-pkg-wireguard-1.0.0.txz
