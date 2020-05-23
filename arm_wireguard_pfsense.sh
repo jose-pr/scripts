92aad7561ecf848bd2d99e97fd10274ffc41640b
jose-pr <dev@alacan.me>
2020-05-23 03:26:54 +0300

    Create arm_wireguard_pfsense.sh

---
+++ arm_wireguard_pfsense.sh
@@ -0,0 +1,4 @@
+pkg install -f bash
+pkg add -f http://pkg.freebsd.org/FreeBSD:11:amd64/quarterly/All/wireguard-go-0.0.20200320.txz
+pkg add -f http://pkg.freebsd.org/FreeBSD:11:amd64/quarterly/All/wireguard-1.0.20200319_2.txz
+pkg add -f https://github.com/Ascrod/pfSense-pkg-wireguard/releases/download/v1.0.0/pfSense-pkg-wireguard-1.0.0.txz
